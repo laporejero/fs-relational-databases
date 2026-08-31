22de54c5a5a684194eeb52b877af31437365eb31
laporejero <laporejero1297@gmail.com>
2026-08-31 20:50:26 +0800

    refactor: Made the table sync sequential

--- models/index.js
+++ models/index.js
@@ -4,11 +4,14 @@ const User = require('./user')
 User.hasMany(Blog)
 Blog.belongsTo(User)
 
-User.sync({ alter: true }).then(() => {
+const syncModels = async () => {
+  await User.sync({ alter: true })
   console.log('User table synchronized')
-})
-Blog.sync({ alter: true }).then(() => {
+
+  await Blog.sync({ alter: true })
   console.log('Blog table synchronized')
-})
+}
+
+syncModels()
 
 module.exports = { Blog, User }
