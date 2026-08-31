f2e46932acaccb15b5cbc9d9ca4db8a7063e32b0
laporejero <laporejero1297@gmail.com>
2026-08-31 13:55:43 +0800

    completed Exercise 14. Better search

--- bloglist-backend/controllers/blogs.js
+++ bloglist-backend/controllers/blogs.js
@@ -9,9 +9,18 @@ router.get('/', async (req, res, next) => {
     const where = {}
 
     if (req.query.search) {
-      where.title = {
-        [Op.substring]: req.query.search
-      }
+      where[Op.or] = [
+        {
+          title: {
+            [Op.substring]: req.query.search
+          }
+        },
+        {
+          author: {
+            [Op.substring]: req.query.search
+          }
+        }
+      ]
     }
 
     const blogs = await Blog.findAll({
