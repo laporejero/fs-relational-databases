bfbb20f2c79f8b4efbfa8ee45053093f68687939
laporejero <laporejero1297@gmail.com>
2026-09-02 10:41:02 +0800

    removed sync

--- models/index.js
+++ models/index.js
@@ -4,14 +4,4 @@ const User = require('./user')
 User.hasMany(Blog)
 Blog.belongsTo(User)
 
-const syncModels = async () => {
-  await User.sync({ alter: true })
-  console.log('User table synchronized')
-
-  await Blog.sync({ alter: true })
-  console.log('Blog table synchronized')
-}
-
-syncModels()
-
 module.exports = { Blog, User }
