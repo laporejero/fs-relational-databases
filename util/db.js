e06d6fab03f72018a0e879319414fe15181a1a41
laporejero <laporejero1297@gmail.com>
2026-09-02 11:49:58 +0800

    completed Exercise 18. Set up migrations

--- util/db.js
+++ util/db.js
@@ -1,23 +1,41 @@
 const Sequelize = require('sequelize')
 const { DATABASE_URL, TEST_DATABASE_URL } = require('./config')
 
+const { Umzug, SequelizeStorage } = require('umzug')
+
 const databaseUrl = process.env.TESTING === 'true'
     ? TEST_DATABASE_URL
     : DATABASE_URL
 
 const sequelize = new Sequelize(databaseUrl, { dialect: 'postgres' })
 
+const runMigrations = async () => {
+    const migrator = new Umzug({
+        migrations: {
+            glob: 'migrations/*.js',
+        },
+        storage: new SequelizeStorage({ sequelize, tableName: 'migrations' }),
+        context: sequelize.getQueryInterface(),
+        logger: console,
+    })
+
+    const migrations = await migrator.up()
+
+    console.log('Migrations up to date', {
+        files: migrations.map((mig) => mig.name),
+    })
+}
+
 const connectToDatabase = async () => {
     try {
         await sequelize.authenticate()
+        await runMigrations()
         console.log('connected to the database')
     } catch (error) {
         console.log('failed to connect to the database')
         console.error(error)
         return process.exit(1)
     }
-
-    return null
 }
 
 module.exports = { connectToDatabase, sequelize }
