90dba0c1f2d2993a8d8d28b41d0a632367cc659a
laporejero <laporejero1297@gmail.com>
2026-09-03 13:13:32 +0800

    completed Exercise 20. Reading list

--- models/index.js
+++ models/index.js
@@ -1,7 +1,11 @@
 const Blog = require('./blog')
 const User = require('./user')
+const ReadingList = require('./readingList')
 
 User.hasMany(Blog)
 Blog.belongsTo(User)
 
-module.exports = { Blog, User }
+User.belongsToMany(Blog, { through: ReadingList, as: 'added_blogs' })
+Blog.belongsToMany(User, { through: ReadingList, as: 'users_added' })
+
+module.exports = { Blog, User, ReadingList }
---
+++ models/readingList.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require('sequelize')
+
+const { sequelize } = require('../util/db')
+
+class ReadingList extends Model {}
+
+ReadingList.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    blog_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: { model: 'blogs', key: 'id' },
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: { model: 'users', key: 'id' },
+    },
+    read: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    }
+}, {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: 'reading_list'
+})
+
+module.exports = ReadingList
